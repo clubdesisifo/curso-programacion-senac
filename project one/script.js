20156abc52b7846ec96ff4f73e6b4c78e260c53e
clubdesisifo <jcgv@hotmail.com>
2026-07-15 11:38:58 -0300

    Añadiendo proyectos uno y dos correctamente

---
+++ project one/script.js
@@ -0,0 +1,38 @@
+        
+       //console.log('teste')
+       //alert('Hello world! 😀😀😀😀😀😀😀😀😀😀😀😀😀😀😀😀😀😀😀😀😀') 
+       //let respuesta = confirm("do you love-me?")
+       //if (respuesta) { alert ("diz que sim")}
+       //else {alert ("CANCEL")}
+       //let a = 3
+       //let b = 10
+       //function soma(){return a + b}
+       //function resta(){return a - b}
+       //console.log(soma())
+       //alert(resta()) /* potencia se expresa a ** b  resto  b % a */
+       //let obj = {
+       //       nome: "Rufino",
+        //      idade: "99"
+        //         }
+       //console.log(obj.nome)
+       //const idade="10 "
+       //console.log(obj.idade)
+        //console.log(idade+obj.idade)
+       //let age = prompt("Digite a sua idade")
+       //let age = Number(prompt("Digite a sua idade"))
+      // if (age<=2){alert("E um bebe")}
+       //else if (age>2 && age<=12){alert ("Crianca")}
+       //else if (age>12 && age<=17){alert("Adolescente")}
+       //else if (age>17 && age<=60){alert("Adulto")}
+       //else {alert ("idoso")}
+       //let nome = prompt("Digite o seu nome")
+       //alert("Hola " + nome + "! seja bem-vindo")
+       //alert(`Hola ${nome}! seja bem-vindo`)
+       //let a = Number(prompt("Digite o primeiro numero"))
+       //let b = Number(prompt("Digite o segundo numero"))
+       //let ResultadoSoma = a + b
+       //alert(`a soma de ${a} + ${b} da ${ResultadoSoma}`)
+       const botao = document.getElementById("tema")
+       botao.addEventListener("click",function(){
+              document.body.classList.toggle("dark")
+       })
---
+++ project one/secret.js
@@ -0,0 +1,54 @@
+// 1. Buscamos el botón y el párrafo usando sus IDs
+//const botao = document.getElementById("revelar")
+//const mensagem = document.getElementById("secreto")
+
+// 2. Escuchamos el clic en el botón
+//botao.addEventListener("click", function() {
+    // 3. Quitamos o ponemos la clase "oculto" al párrafo
+ //   mensagem.classList.toggle("oculto")
+//})
+//let lista = ["parafuso","pneu","aro","tinta", "macaneta"]
+//for (let item of lista){console.log(item)}
+//for (let item in lista){console.log(item)}
+//for (let item of lista){console.log(item[2])}
+//for (let item of lista){console.log(item[6])}
+// item es variable de control que recibe un nuvo valor ac ada interaccion
+
+
+//for (let i=1;i<=100;i++){console.log(i)}
+//for (let i=100;i>=1;i=i-2){console.log(i)}
+//for (let i=0; i<=50;i++){if (i%2 === 1){console.log(i)}}
+//for (let i = 2; i <= 100; i++) {
+ // let esPrimo = true;
+
+  // Probamos si "i" se puede dividir por algún número más chico
+ //for (let j = 2; j < i; j++) {
+  //  if (i % j === 0) {
+ //     esPrimo = false; // Si se puede dividir, ya no es primo
+  //    break;           // Dejamos de buscar para ahorrar tiempo
+  //  }
+ // }
+
+  // Si después de probar con todos ningún número lo dividió, lo mostramos
+  //if (esPrimo) {
+   // console.log(i);
+  //}
+//}
+//let number=Number(prompt("Digite aqui o numero"))
+//for (let i=1;i<=10;i++){console.log(`${number} x ${i} = ${number*i}`)}
+
+// soma += i   === soma = soma = i
+//let juan="jose"
+//if (true) {
+ //   var saludo = "Hola";
+  //   juan = "holita";
+
+//}
+//console.log(saludo);
+//console.log(juan);
+let clodovir = prompt("digite o seu nome")
+for (let i=0;i<clodovir.length;i++){console.log(clodovir[i])}
+console.log(clodovir.length)
+
+
+
